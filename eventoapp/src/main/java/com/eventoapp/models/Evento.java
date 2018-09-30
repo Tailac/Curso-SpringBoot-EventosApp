0ad25837cee7c0f36d15791028590c66c3556d83
Taila Saviani <tailanayara@gmail.com>
2018-09-30 00:13:05 -0300

    Configuracao do Banco de Dados Mysql com JPA e Hibernate

--- eventoapp/src/main/java/com/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/models/Evento.java
@@ -1,6 +1,22 @@
 package com.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javassist.SerialVersionUID;
+
+@Entity
+public class Evento implements Serializable {
+	
+	private static final long SerialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
 	
 	private String nome;
 	private String local;
@@ -8,6 +24,12 @@ public class Evento {
 	private String horario;
 	
 	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
