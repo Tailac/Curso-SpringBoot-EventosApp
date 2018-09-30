8c5afd18d237b4011b7281e9afb8e8793bf9e185
Taila Saviani <tailanayara@gmail.com>
2018-09-30 01:58:25 -0300

    Implementação da busca de eventos no banco de dados

--- eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoapp.models.Evento;
 import com.eventoapp.repository.EventoRepository;
@@ -24,5 +25,13 @@ public class EventoController {
 		
 		er.save(evento);
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos(){
+		ModelAndView mv = new ModelAndView("/index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos",eventos);
+		return mv;
 	}
 }
