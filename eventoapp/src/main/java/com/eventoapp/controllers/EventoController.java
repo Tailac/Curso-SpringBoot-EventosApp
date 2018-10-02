58fcefc70b4388af4b3546c12adec9b70bbc500b
Taila Saviani <tailanayara@gmail.com>
2018-10-02 09:43:36 -0300

    Layout com Materialize e Criação da tela Detalhes de evento

--- eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -2,6 +2,7 @@ package com.eventoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,5 +34,14 @@ public class EventoController {
 		Iterable<Evento> eventos = er.findAll();
 		mv.addObject("eventos",eventos);
 		return mv;
+	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		return mv;
+		
 	}
 }
