0d998bbcabb42397d6aa681667f9215252926561
Taila Saviani <tailanayara@gmail.com>
2018-10-02 09:58:32 -0300

    Criação Entidade Convidado e alteração tela DetalhesEvento

--- eventoapp/src/main/java/com/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/models/Evento.java
@@ -1,11 +1,13 @@
 package com.eventoapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import javassist.SerialVersionUID;
 
@@ -23,6 +25,8 @@ public class Evento implements Serializable {
 	private String data;
 	private String horario;
 	
+	@OneToMany
+	private List<Convidado> convidados;
 	
 	public long getCodigo() {
 		return codigo;
